5e28ad81bfb24b0f34a038c2c530417318970209
Alessia :/ <114771570+Ale417@users.noreply.github.com>
2023-11-15 09:53:33 +0100

    .

---
+++ Trainline/Trainline/TrainlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainlineApp.swift
+//  Trainline
+//
+//  Created by Alessia Previdente on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TrainlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
